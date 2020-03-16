8e59cc18059f7664d5c777cce61f1bb33ece9e67
mustafa <tuuranmustafa@gmail.com>
2020-03-16 18:33:36 +0300

    page upgraded

--- assets/jsfiles/app.js
+++ assets/jsfiles/app.js
@@ -2,11 +2,16 @@ window.onload = function(){
 
 
 this.setTimeout(function(){
-  $(".animation-pieces img").css("opacity","1")
+
+  $(".animation-pieces img").css({
+    "opacity" :"1",
+  });
+  $("#fade-word").css("opacity","1").addClass("scaleanimation");
+  $("#fade-word2 span").addClass("addTextDecoration");
   var tl = gsap.timeline();
-tl.from(".img1", {rotation: 360, x: 1400, y:-800, duration: 1.2, ease: Back.easeOut.config(1.7)})
-  .from(".img2", {rotation: 720, x: 1200, y:1500, duration: 1.5, ease: Back.easeOut.config(1.7)})
-  .from(".img3", {rotation: 360, x: 1400, y:-1000, duration: 1, ease: Back.easeOut.config(1.7)})
-  .from(".img4", {rotation: 1080, x: 1400, y:1000, duration: 1.5, ease: Back.easeOut.config(1.7)})
+tl.from(".img1", {rotation: 360, x: 1400,  duration: 1.2, ease: Back.easeOut.config(1.7)})
+  .from(".img2", {rotation: 720, x: 1200,  duration: 1.5, ease: Back.easeOut.config(1.7)})
+  .from(".img3", {rotation: 1080, x: 1400,  duration: 1, ease: Back.easeOut.config(1.7)})
+  .from(".img4", {rotation: 1440, x: 1400, duration: 1.5, ease: Back.easeOut.config(1.7)})
 },1000)  
 }
